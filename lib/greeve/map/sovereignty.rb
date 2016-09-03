d0f7a575e1f05edf254d5dcdc74222e54f140ddf
Alex McLain <alex@alexmclain.com>
2016-09-03 13:52:01 -0700

    Implement Map::Sovereignty endpoint.

---
+++ lib/greeve/map/sovereignty.rb
@@ -0,0 +1,25 @@
+require_relative "../base_item"
+
+module Greeve
+  module Map
+    # Provides sovereignty / ownership information for solar systems in New Eden,
+    # excluding wormhole space.
+    #
+    # @see https://eveonline-third-party-documentation.readthedocs.io/en/latest/xmlapi/map/map_sovereignty.html
+    class Sovereignty < Greeve::BaseItem
+      endpoint "map/Sovereignty"
+
+      rowset :solar_systems, xpath: "eveapi/result/rowset[@name='solarSystems']" do
+        attribute :solar_system_id,   xpath: "@solarSystemID",   type: :integer
+        attribute :alliance_id,       xpath: "@allianceID",      type: :integer
+        attribute :faction_id,        xpath: "@factionID",       type: :integer
+        attribute :corporation_id,    xpath: "@corporationID",   type: :integer
+        attribute :solar_system_name, xpath: "@solarSystemName", type: :string
+      end
+
+      def initialize(opts = {})
+        super(opts)
+      end
+    end
+  end
+end
