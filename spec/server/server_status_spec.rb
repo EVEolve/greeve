a662c0d6c78aa80b2ad246767b8e22a09746c7e3
Alex McLain <alex@alexmclain.com>
2016-10-08 20:11:43 -0700

    Convert Server::ServerStatus spec to VCR.

--- spec/server/server_status_spec.rb
+++ spec/server/server_status_spec.rb
@@ -1,13 +1,8 @@
-describe Greeve::Server::ServerStatus do
-  let(:base_endpoint) { "#{Greeve::EVE_API_BASE_URL}/server/ServerStatus.xml.aspx" }
-  let(:xml_filename) { "server/server_status" }
+vcr_opts = {
+  cassette_name: "server/server_status",
+}
 
-  before {
-    stub_endpoint(base_endpoint, xml_filename)
-
-    invalidate_remaining_endpoints
-  }
-
+describe Greeve::Server::ServerStatus, vcr: vcr_opts do
   subject { Greeve::Server::ServerStatus.new }
 
   its(:server_open) { should eq true }
