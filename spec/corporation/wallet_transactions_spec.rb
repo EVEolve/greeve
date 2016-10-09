3a24b119da633078ba98cf7641cf4bc92b040549
Alex McLain <alex@alexmclain.com>
2016-10-08 22:08:42 -0700

    Convert Corporation::WalletTransactions spec to VCR.

--- spec/corporation/wallet_transactions_spec.rb
+++ spec/corporation/wallet_transactions_spec.rb
@@ -1,14 +1,12 @@
-describe Greeve::Corporation::WalletTransactions do
-  let(:base_endpoint) { "#{Greeve::EVE_API_BASE_URL}/corp/WalletTransactions.xml.aspx" }
-  let(:xml_filename) { "corporation/wallet_transactions" }
+vcr_opts = {
+  cassette_name: "corporation/wallet_transactions",
+}
 
-  before {
-    stub_endpoint(base_endpoint, xml_filename)
+describe Greeve::Corporation::WalletTransactions, vcr: vcr_opts do
+  let(:key) { "1515664" }
+  let(:vcode) { "QYYBHdsFMmdWjc9bkWhqqKx00NLqA1c3pNHlacqHUGpaTkrnyrzwZ0vFY9L6aei3" }
 
-    invalidate_remaining_endpoints
-  }
-
-  let(:resource) { Greeve::Corporation::WalletTransactions.new }
+  let(:resource) { Greeve::Corporation::WalletTransactions.new(key: key, vcode: vcode) }
 
   context "resource" do
     subject { resource }
@@ -37,12 +35,15 @@ describe Greeve::Corporation::WalletTransactions do
     its(:client_type_id) { should eq 1377 }
 
     context "with option" do
-      context "account_key" do
-        let(:xml_filename) { "corporation/wallet_transactions_1006" }
+      vcr_opts_with_account_key = {
+        cassette_name: "corporation/wallet_transactions_account_1006",
+      }
+
+      context "account_key", vcr: vcr_opts_with_account_key do
         let(:account_key) { 1006 }
 
         let(:resource) {
-          Greeve::Corporation::WalletTransactions.new(account_key: account_key)
+          Greeve::Corporation::WalletTransactions.new(account_key: account_key, key: key, vcode: vcode)
         }
 
         its(:transaction_date_time) { should eq Time.parse("2016-08-27 17:55:05 UTC") }
@@ -63,14 +64,47 @@ describe Greeve::Corporation::WalletTransactions do
         its(:client_type_id) { should eq 1377 }
       end
 
-      context "from_id" do
-        # Depends on issue #109 being implemented so query params can be tested.
-        xspecify
+      vcr_opts_with_from_id = {
+        cassette_name: "corporation/wallet_transactions_from_id",
+      }
+
+      context "from_id", vcr: vcr_opts_with_from_id do
+        let(:from_id) { 4389147674 }
+
+        let(:resource) {
+          Greeve::Corporation::WalletTransactions.new(from_id: from_id, key: key, vcode: vcode)
+        }
+
+        its(:transaction_date_time) { should eq Time.parse("2016-08-24 04:07:37 UTC") }
+        its(:transaction_id) { should eq 4389147672 }
+        its(:quantity) { should eq 2 }
+        its(:type_name) { should eq "Mining Laser Upgrade I" }
+        its(:type_id) { should eq 22542 }
+        its(:price) { should eq BigDecimal.new("36228.80") }
+        its(:client_id) { should eq 96209930 }
+        its(:client_name) { should eq "Chris Huntington" }
+        its(:character_id) { should eq 462421468 }
+        its(:character_name) { should eq "Zaphoon" }
+        its(:station_id) { should eq 60011866 }
+        its(:station_name) { should eq "Dodixie IX - Moon 20 - Federation Navy Assembly Plant" }
+        its(:transaction_type) { should eq "buy" }
+        its(:transaction_for) { should eq "corporation" }
+        its(:journal_transaction_id) { should eq 12984444965 }
+        its(:client_type_id) { should eq 1377 }
       end
 
-      context "row_count" do
-        # Depends on issue #109 being implemented so query params can be tested.
-        xspecify
+      vcr_opts_with_row_count = {
+        cassette_name: "corporation/wallet_transactions_row_count",
+      }
+
+      context "row_count", vcr: vcr_opts_with_row_count do
+        let(:row_count) { 1 }
+
+        let(:resource) {
+          Greeve::Corporation::WalletTransactions.new(row_count: row_count, key: key, vcode: vcode)
+        }
+
+        specify { resource.transactions.count.should eq row_count }
       end
     end
   end
