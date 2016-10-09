f9de0d29ab72ff6d906cd4680c6329435803c0d7
Alex McLain <alex@alexmclain.com>
2016-10-08 21:11:01 -0700

    Convert Character::SkillInTraining spec to VCR.

--- spec/character/skill_in_training_spec.rb
+++ spec/character/skill_in_training_spec.rb
@@ -1,15 +1,11 @@
-describe Greeve::Character::SkillInTraining do
+vcr_opts = {
+  cassette_name: "character/skill_in_training",
+}
+
+describe Greeve::Character::SkillInTraining, vcr: vcr_opts do
   let(:key) { "1515664" }
   let(:vcode) { "QYYBHdsFMmdWjc9bkWhqqKx00NLqA1c3pNHlacqHUGpaTkrnyrzwZ0vFY9L6aei3" }
-  let(:base_endpoint) { "#{Greeve::EVE_API_BASE_URL}/char/SkillInTraining.xml.aspx" }
-  let(:xml_filename) { "character/skill_in_training" }
   let(:character_id) { 462421468 }
-
-  before {
-    stub_endpoint(base_endpoint, xml_filename)
-
-    invalidate_remaining_endpoints
-  }
 
   subject {
     Greeve::Character::SkillInTraining.new(character_id, key: key, vcode: vcode)
