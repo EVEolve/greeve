86b8f777db24a5e4a3997994d40162c34544b480
Alex McLain <alex@alexmclain.com>
2016-10-08 20:33:32 -0700

    Convert Map::Jumps spec to VCR.

--- spec/map/jumps_spec.rb
+++ spec/map/jumps_spec.rb
@@ -1,13 +1,8 @@
-describe Greeve::Map::Jumps do
-  let(:base_endpoint) { "#{Greeve::EVE_API_BASE_URL}/map/Jumps.xml.aspx" }
-  let(:xml_filename) { "map/jumps" }
+vcr_opts = {
+  cassette_name: "map/jumps",
+}
 
-  before {
-    stub_endpoint(base_endpoint, xml_filename)
-
-    invalidate_remaining_endpoints
-  }
-
+describe Greeve::Map::Jumps, vcr: vcr_opts do
   let(:resource) { Greeve::Map::Jumps.new }
 
   context "resource" do
